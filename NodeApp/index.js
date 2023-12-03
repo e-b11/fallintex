c54d1af738cffeaaf9059f8e3322da758dceba5e
e-b11 <96320276+e-b11@users.noreply.github.com>
2023-12-03 12:15:24 -0700

    start CRUD functionality

--- NodeApp/index.js
+++ NodeApp/index.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+//Connect css file
+app.use(express.static(__dirname + "/public"));
+
 //Set up knex, will have to adjust database
 const knex = require("knex")({
   client: "pg",
@@ -24,6 +27,47 @@ const knex = require("knex")({
     ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
   },
 });
+
+// READ FUNCTIONALITY (to be changed)
+// app.get("/", (req, res) => {
+//   knex
+//     .select("band_name", "lead_singer")
+//     .from("bands")
+//     .then((bands) => {
+//       res.render("displayBand", { mybands: bands });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json({ err });
+//     });
+// });
+
+//UPDATE FUNCTIONALITY (to be changed)
+// app.post("/editBand", (req, res) => {
+//   knex("bands").where("band_id", parseInt(req.body.bandID)).update({
+//       band_name: req.body.bandName,
+//       lead_singer: req.body.singer
+//   }).then(mybands => {
+//       res.redirect("/");
+//   });
+// });
+
+//CREATE FUNCTIONALITY (to be changed)
+// app.post("/addBand", (req, res) => {
+//   knex("bands").insert(req.body).then(mybands => {
+//       res.redirect("/");
+//   })
+// });
+
+//DELETE FUNCTIONALITY
+// app.post("/deleteBand/:id", (req, res) => {
+//   knex("bands").where("band_id", req.params.id).del().then(mybands => {
+//       res.redirect("/");
+//   }).catch(err => {
+//       console.log(err);
+//       res.status(500).json({err});
+//   })
+// });
 
 //Set up port/listening
 app.listen(port, () => console.log("Website started."));
